35dfecd19858c0fb94c4b3f824155cfd6825ba9d
piumosso <golikoffartem@gmail.com>
2013-04-24 21:54:30 +0400

    Tests for Logger

--- specs/logger.spec.js
+++ specs/logger.spec.js
@@ -1,8 +1,40 @@
-var UserStory = require('../lib/UserStory').UserStory;
+var Logger = require('../lib/Logger').Logger;
 
 
-describe('Logger', function(){
-    it('should ', function(){
-        expect(true).toEqual(true);
+describe('Logger without restrictions', function(){
+    var sayMock = jasmine.createSpy(),
+        logger = new Logger({
+            say: sayMock
+        });
+
+    it('should tell all logs', function(){
+        logger.log('Hi!', 'app.section');
+        expect(sayMock).toHaveBeenCalledWith('Hi!', 'app.section');
+    });
+});
+
+
+describe('Logger with restrictions', function(){
+    var sayMock = jasmine.createSpy(),
+        logger = new Logger({
+            say: sayMock,
+            tell: ['a.b', 'k', 'k.l'],
+            shutUp: ['a.b.c', 'k', 'x']
+        });
+
+    it('should tell only allowed logs except sections in shutUp', function(){
+        logger.log('Msg', 'a.b');
+        expect(sayMock).toHaveBeenCalled();
+        logger.log('Msg', 'a.b.d');
+        expect(sayMock).toHaveBeenCalled();
+        logger.log('Msg', 'a.b.c');
+        expect(sayMock).not.toHaveBeenCalled();
+    });
+
+    it('should not tell shutUp logs', function(){
+        logger.log('Msg', 'k');
+        expect(sayMock).not.toHaveBeenCalled();
+        logger.log('Msg', 'k.l');
+        expect(sayMock).not.toHaveBeenCalled();
     });
 });
